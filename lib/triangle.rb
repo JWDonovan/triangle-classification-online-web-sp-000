187e235f09ba2ee21e76b60224f2e77ff15e1180
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 17:17:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,8 +23,11 @@ class Triangle
     triangle = [(a + b > c), (a + c > b), (b + c > a)]
 
     [a, b, c].each do |side|
-      triangle << false if side <= 0
-      raise TriangleError if triangle.include?(false)
+      # triangle << false if side <= 0
+      # raise TriangleError if triangle.include?(false)
+      if side <= 0
+        raise TriangleError
+      end
     end
   end
 
